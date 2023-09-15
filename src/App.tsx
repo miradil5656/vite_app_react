12122e32af4a64cac2ed66b9a1ff4828c6c9357f
Miradil <miradild404@gmail.com>
2023-09-15 17:02:58 +0900

    add propt

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,14 @@ import Main from "./components/Main"
 
 
 const App = () => {
+
+  const items = ["Home", "Menu", "Acsess", "Contact Us"]
   return (
     <>
     <nav className="bg-black text-red-500 flex p-4 justify-between">
       <h1 className="font-bold">Logo</h1>
-      <ul className="flex gap-4 ">
-        <li className="hover:cursor-pointer font-bold">Home</li>
-        <li className="hover:cursor-pointer ">Menu</li>
-        <li className="hover:cursor-pointer ">Acsess</li>
-        <li className="hover:cursor-pointer ">Contact Us</li>
-        
-      </ul>
     </nav>
-    <Main/>
+    <Main manu = {items} title="Menu"/>
     <footer></footer>
     </>
   )
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,18 +1,24 @@
 import { useState } from "react"
+interface Props {
+    manu: string[]
+    title: string
+}
 
-const Main = () => {
+const Main = ({manu,title}:Props) => {
 
 const [selectedItem, setSelectedItem] = useState(-1)
-const items = ["Home", "Menu", "Acsess", "Contact Us"]
-const style = "bg-green-500 text-center p-4 rounded-lg text-white text-2xl"
+
+const style = "w bg-green-500 text-center p-4 rounded-lg text-white text-2xl "
 const style1 = "bg-red-500 text-center p-4 rounded-lg text-xl"
 // const handleClcik =(clickitem:MouseEvent)=>{
 //     console.log(`${clickitem} cliked`)
 // }
   return (
     <div>
-        <ul className="flex gap-8  flex-col  p-4">
-            {items.map((item,index) => (
+         <h1>{title}</h1>
+        <ul className="flex gap-8  flex-col bg-slate-400 justify-center  p-4 md:flex-row">
+           
+            {manu.map((item,index) => (
                 //fix the key error
 
                 <li onClick={()=>setSelectedItem(index)} key={item} className={selectedItem === index ? style :style1 }>{item}</li>
